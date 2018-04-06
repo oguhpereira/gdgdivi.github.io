2aeeceaffa0127c5db58b7890c576707d2ace5d5
Vinícius Soares <squimer@gmail.com>
2018-04-06 19:21:20 -0300

    Updates

--- gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components', 'gatsby-plugin-sass']
-};
